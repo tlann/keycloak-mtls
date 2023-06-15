6bd75afb7db91d1b9edf8f1f0d14c7328a3f7968
Thomas Harper <thomas.l.harper.ctr@navy.mil>
2023-06-15 10:19:11 -0700

    Add generation of pks12 for keystore.jks as well as making sure all of the appropriate certs get added Cleanup compose.yml and convert some settings to command parameters vs env vars

--- certs/x509.sh
+++ certs/x509.sh
@@ -2,20 +2,27 @@
 
 #lovingly inspired from https://gist.github.com/dasniko/b1aa115fd9078372b03c7a9f7e9ec189
 
+PW=changeit
+
 echo "*********"
 echo "RootCA"
 echo "*********"
-openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 -keyout rootCA.key -out rootCA.crt
+openssl req -x509  -passin pass:$PW -sha256 -days 3650 -newkey rsa:4096 -keyout rootCA.key -out rootCA.crt 
 
 echo "*********"
 echo "Host certificate csr"
 echo "*********"
-openssl req -new -newkey rsa:4096 -keyout localhost.key -out localhost.csr -nodes
+openssl req -new -newkey rsa:4096  -passin pass:$PW -keyout localhost.key -out localhost.csr -nodes 
 
 echo "*********"
 echo "Sign host csr with rootCA (see below for file localhost.ext):"
 echo "*********"
-openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in localhost.csr -out localhost.crt -days 365 -CAcreateserial -extfile localhost.ext
+openssl x509 -req  -passin pass:$PW -CA rootCA.crt -CAkey rootCA.key -in localhost.csr -out localhost.crt -days 365 -CAcreateserial -extfile localhost.ext
+
+echo "*********"
+echo "Create pkcs12 file for server "
+echo "*********"
+openssl pkcs12 -export -in localhost.crt -inkey localhost.key -out keystore.p12 -name "server certificate" -chain -CAfile rootCA.crt -caname "self signed ca certificate" -passin pass:$PW -passout pass:$PW
 
 echo "*********"
 echo "Client (user) certificate"
@@ -28,20 +35,20 @@ echo "*********"
 openssl x509 -req -CA rootCA.crt -CAkey rootCA.key -in fredFlintstone.csr -out fredFlintstone.crt -days 365 -CAcreateserial
 
 echo "*********"
-echo "Import client key and crt in keystore to create the "certificate" to be used in the browser:"
+echo "Import client key and crt in keystore to create the certificate to be used in the browser:"
 echo "*********"
 openssl pkcs12 -export -out fredFlintstone.p12 -name "fredFlintstone" -inkey fredFlintstone.key -in fredFlintstone.crt
 
 echo "*********"
 echo "Create a keystore using keytool"
 echo "*********"
-keytool -genkey -alias host -keyalg RSA -keystore keystore.jks -validity 10950
+
+keytool -importkeystore -destkeystore keystore.jks -srckeystore keystore.p12 -srcstoretype PKCS12 -alias "server certificate" -srcstorepass $PW -deststorepass $PW
 
 echo "*********"
 echo "Create a truststore using keytool"
 echo "*********"
 keytool -importcert -alias HOSTDOMAIN -keystore truststore.jks -file rootCA.crt
-
 
 
 mkdir ../genCerts
